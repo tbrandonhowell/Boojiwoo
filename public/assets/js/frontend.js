5205b02a31fe66a7fa7cdb338ece0515dc3f1bf0
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-04-02 21:24:05 -0400

    20190402 21:24

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -62,13 +62,70 @@ window.onload = function() {
             // trigger sad modal
             $('#noneYetModal').modal().show();
         } else {
-            // trigger happy modal
-            $("#taskModalLongTitle").text(taskConfirmedName);
-            $('#taskModal').modal().show();
+            // update the DB
+            const userId = $(this).data('userId');
+            const taskId = $(this).data('taskId');
+            $.ajax({
+                url: "api/routes/completeTask/" + userId + "/" + taskId,
+                type: "post"
+            }).then( function() {
+                // trigger happy modal
+                $("#taskModalLongTitle").text(taskConfirmedName);
+                $('#taskModal').modal().show();
+                $(this).remove();
+                $("#none").remove();
+                // $(this).remove();
+                // location.reload(); // reload the page
+            })
+            
         }
-        // trigger div w/ task info in it using variable
-        // ajax call to DB to mark task as complete
     });
+
+    //  router.post('/purchaseSwag/:userId/:swagId', ensureAuthenticated, AppController.purchaseSwag);
+    // capture the click on a purchase
+    $(".buy-button").on("click", function (event) {
+        event.preventDefault();
+        const userPoints = $(this).data('userPoints');
+        const costPoints = $(this).data('costPoints');
+        if (userPoints >= costPoints) {
+            const userId = $(this).data('userId'); // this might go away
+            const swagId = $(this).data('swagId');
+            $.ajax({
+                url: "api/routes/purchaseSwag" + userId + "/" + swagId,
+                type: "post"
+            }).then( function() {
+                location.reload(); // reload the page
+            })
+        } else {
+            // TODO: display a message that they need to earn X more points
+        }
+    });
+
+    //  router.post('/updateAvatar/:userId/:type/:swagId', ensureAuthenticated, AppController.updateAvatar);
+    // capture the click on an avatar update
+    $(".update").on("click", function (event) {
+        event.preventDefault();
+        const swagType = $(this).data('swagType'); // this needed for API route build-out
+        const fileName = $(this).data('fileName'); // this needed to update DOM avatar
+        const userId = $(this).data('userId'); // this might go away
+        const swagId = $(this).data('swagId'); // needed to update DB values
+        $.ajax({
+            url: "api/routes/updateAvatar/" + userId + "/" + swagType + "/" + swagId,
+            type: "post"
+        }).then( function() {
+            // mouth eyes outfit body << swagIDs
+            // new-eyes, etc outfit eyes mouth body
+            if (swagType === "mouth") {
+                $("#new-mouth").attr("src",fileName);
+            } else if (swagType === "eyes") {
+                $("#new-eyes").attr("src",fileName);
+            } else if (swagType === "outfit") {
+                $("#new-outfit").attr("src",fileName);
+            } else if (swagType === "body") {
+                $("#new-body").attr("src",fileName);
+            }
+        });
+    })
 
 
     // boojiwoo clearfix but w/ js
